d6780871340ee9ca19516dc08c74463b7d60cb6d
MozartOnWeb <mozartontheweb@gmail.com>
2022-10-31 18:36:07 +0000

    extra & icons

--- sections/Welcome/welcome.tsx
+++ sections/Welcome/welcome.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Image from "next/image";
+
+import waves from "../../assets/extra/welcomeWaves.svg";
+
 export const Welcome = () => {
-  return <section id="welcome">Welcome</section>;
+  return (
+    <section id="welcome">
+      <div>
+        <Image src={waves} alt="waves" />
+      </div>
+    </section>
+  );
 };
 
 export default Welcome;
