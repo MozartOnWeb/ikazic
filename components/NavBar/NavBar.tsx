7692e9a3f1c60af9dd0e627e0b84e10f726f6a73
MozartOnWeb <mozartontheweb@gmail.com>
2022-10-31 14:01:45 +0000

    navbar setup

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,5 +1,79 @@
+import Image from "next/image";
+import { Link } from "react-scroll";
+
+//import logo
+import logo from "../../assets/extra/logo.png";
+
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <nav>
+      <div className="logo">
+        <Image src={logo} alt="ikazic logo" />
+      </div>
+      <div className="links">
+        <Link
+          activeClass="active"
+          to="home"
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-200}
+        >
+          Acceuil
+        </Link>
+        <Link
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-100}
+        >
+          A propos
+        </Link>
+        <Link
+          activeClass="active"
+          to="features"
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-100}
+        >
+          Fonctionnalités
+        </Link>
+        <Link
+          activeClass="active"
+          to="goals"
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-100}
+        >
+          Objectifs
+        </Link>
+        <Link
+          activeClass="active"
+          to="faqs"
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-100}
+        >
+          FAQs
+        </Link>
+      </div>
+      <Link
+        to="newsletter"
+        spy={true}
+        smooth={true}
+        offset={-100}
+        duration={500}
+        className="goToNewsletter"
+      >
+        <span>Newsletter</span>
+      </Link>
+    </nav>
+  );
 };
 
 export default NavBar;
