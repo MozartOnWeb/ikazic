bc4ddf66cd3d1296f7af71c4179000eacf991eef
MozartOnWeb <mozartontheweb@gmail.com>
2022-11-01 00:16:51 +0000

    mockups files added

--- sections/Welcome/welcome.tsx
+++ sections/Welcome/welcome.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 
-import Image from "next/image";
-
-import waves from "../../assets/extra/welcomeWaves.svg";
-
 export const Welcome = () => {
-  return (
-    <section id="welcome">
-      <div>
-        <Image src={waves} alt="waves" />
-      </div>
-    </section>
-  );
+  return <section id="welcome"></section>;
 };
 
 export default Welcome;
