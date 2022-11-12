a7c485296aac71d81e646d3865662fb603f557f2
MozartOnWeb <mozartontheweb@gmail.com>
2022-11-12 22:40:33 +0000

    working on footer section

--- sections/Footer/footer.tsx
+++ sections/Footer/footer.tsx
@@ -1,5 +1,80 @@
+import Image from "next/image";
+
+import { Link } from "react-scroll";
+
+//import ikazic logo
+import Logo from "../../assets/extra/logo.png";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div id="footer">
+      <div className="content">
+        <div className="ikazic">
+          <Image src={Logo} alt="ikazic logo" />
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Exercitationem, dolore similique? Minima, voluptatum nostrum nulla
+            facilis ea, quibusdam voluptate molestiae.
+          </p>
+        </div>
+
+        <div className="details">
+          <h3>Liens</h3>
+          <Link
+            activeClass="active"
+            to="welcome"
+            spy={true}
+            smooth={true}
+            duration={500}
+            offset={-200}
+          >
+            Acceuil
+          </Link>
+          <Link
+            activeClass="active"
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={500}
+            offset={-200}
+          >
+            A propos
+          </Link>
+          <Link
+            activeClass="features"
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={500}
+            offset={-200}
+          >
+            Fonctionnalités
+          </Link>
+        </div>
+
+        <div className="details">
+          <h3>Contacts</h3>
+          <p>Badalabougou, Bamako, Mali</p>
+          <p>ikazic@gmail.com</p>
+          <p>00223-78437323</p>
+        </div>
+
+        <div className="details">
+          <h3>A propos de nous</h3>
+          <p>L&apos;équipe</p>
+          <p>Notre Histoire</p>
+          <p>00223-78437323</p>
+        </div>
+      </div>
+      <div className="bottom-section">
+        <div className="divider"></div>
+        <div className="copyright-content">
+          <span>Copyright2022</span>
+          <span>ikazic.ml tout droit réservé</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
