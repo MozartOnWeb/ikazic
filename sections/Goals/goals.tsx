0f06c9010106f248716e102deb9f44e994cb8c3f
MozartOnWeb <mozartontheweb@gmail.com>
2022-11-15 20:56:45 +0000

    working on goals section

--- sections/Goals/goals.tsx
+++ sections/Goals/goals.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Image from "next/image";
+
+//import mockups
+import artistMock from "../../assets/mockups/artist.png";
 
 export const Goals = () => {
   return (
@@ -10,6 +14,61 @@ export const Goals = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
           corrupti eaque aspernatur sed perferendis animi?
         </p>
+      </div>
+      <div className="bottom-section">
+        {/* bottom-left section */}
+        <div className="bottom-left-right left">
+          {/* top part */}
+          <div className="part">
+            <div className="number">1</div>
+            <h4>Pour les artistes</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+              eos, ipsam doloribus nostrum ex iste.
+            </p>
+          </div>
+          {/* bottom part */}
+          <div className="part">
+            <div className="number number-red">3</div>
+            <h4>Pour le Mali</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+              eos, ipsam doloribus nostrum ex iste.
+            </p>
+          </div>
+        </div>
+        {/* bottom-left section */}
+
+        {/* bottom-center section */}
+        <div className="bottom-center">
+          <div className="colored-bg"></div>
+          <div className="mockup-container">
+            <Image src={artistMock} alt="ikazic artist profile screen" />
+          </div>
+        </div>
+        {/* bottom-center section */}
+
+        {/* bottom-right section */}
+        <div className="bottom-left-right right">
+          {/* top part */}
+          <div className="part">
+            <div className="number number-red">2</div>
+            <h4>Pour les utilisateurs</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+              eos, ipsam doloribus nostrum ex iste.
+            </p>
+          </div>
+          {/* bottom part */}
+          <div className="part">
+            <div className="number">4</div>
+            <h4>Pour l&apos;Afrique</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+              eos, ipsam doloribus nostrum ex iste.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
